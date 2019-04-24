c877bcd488651560357d9e42fdb6e8d47fe1100c
Alex Mota <alexmota5@hotmail.com>
2019-04-23 22:24:43 -0300

    add hystrix

---
+++ estoque/src/main/resources/db/migration/V1.0__CREATE_TABLES.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------------------
+-- Table `db_srm`.`srm_product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_srm`.`srm_product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(400) NULL,
+  `bar_code` VARCHAR(200) NULL,
+  `department` VARCHAR(100) NULL,
+  `amount` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
